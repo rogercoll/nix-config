0abf9d88df18caefc2529a9b820d0506a9733733
Roger Coll <rogercoll@protonmail.com>
2024-04-25 10:52:24 +0200

    use default-dark theme for tmux

--- home-manager/features/tmux.nix
+++ home-manager/features/tmux.nix
@@ -1,57 +1,63 @@
 { pkgs, ... }:
-let
-    tmux-sensible = pkgs.tmuxPlugins.mkTmuxPlugin
-    {
-      pluginName = "tmux-sensible";
-      version = "unstable-2023-01-06";
-      src = pkgs.fetchFromGitHub {
-        owner = "tmux-plugins";
-        repo = "tmux-sensible";
-        rev = "25cb91f42d020f675bb0a2ce3fbd3a5d96119efa";
-        sha256 = "sha256-sw9g1Yzmv2fdZFLJSGhx1tatQ+TtjDYNZI5uny0+5Hg=";
-      };
-    };
-    base16-tmux = pkgs.tmuxPlugins.mkTmuxPlugin
-    {
-      pluginName = "base16-tmux";
-      version = "abc93bd53b3920569de9b7e7f747c21e1590bfad";
-      src = pkgs.fetchFromGitHub {
-        owner = "tinted-theming";
-        repo = "base16-tmux";
-        rev = "abc93bd53b3920569de9b7e7f747c21e1590bfad";
-        sha256 = "sha256-DtNWFc0SSl2vSdUBlB40WWhlf+UkokvxOtOa/g/xzDY=";
-      };
-    };
-  in
-  {
-    programs.tmux = {
-      enable = true;
-      prefix = "C-t";
-      baseIndex = 1;
-      mouse = true;
+{
+  programs.tmux = {
+    enable = true;
+    prefix = "C-t";
+    baseIndex = 1;
+    mouse = true;
 
-      # For vi mode
-      keyMode = "vi";
-      customPaneNavigationAndResize = true; # For vi mode
-      escapeTime = 0; # We do not use ESC for any tmux keybindings
+    # For vi mode
+    keyMode = "vi";
+    customPaneNavigationAndResize = true; # For vi mode
+    escapeTime = 0; # We do not use ESC for any tmux keybindings
 
-      extraConfig = ''
-        set-option -sa terminal-overrides ",xterm*:Tc"
-        # Open new windows in current pane path
-        bind  c  new-window      -c "#{pane_current_path}"
-        bind  %  split-window -h -c "#{pane_current_path}"
-        bind '"' split-window -v -c "#{pane_current_path}"
-      '';
+    extraConfig = ''
+      set-option -sa terminal-overrides ",xterm*:Tc"
+      # Open new windows in current pane path
+      bind  c  new-window      -c "#{pane_current_path}"
+      bind  %  split-window -h -c "#{pane_current_path}"
+      bind '"' split-window -v -c "#{pane_current_path}"
 
-      plugins = with pkgs; [
-        {
-          plugin = tmux-sensible;
-        }
-        {
-          plugin = base16-tmux;
-          extraConfig = "set -g @colors-base16 'default-dark'";
-        }
-        tmuxPlugins.vim-tmux-navigator
-      ];
-    };
-  }
+      # COLORIZE: https://github.com/tinted-theming/tinted-tmux/blob/main/colors/base16-default-dark.conf
+      # default statusbar colors
+      set-option -g status-style "fg=#b8b8b8,bg=#282828"
+
+      # default window title colors
+      set-window-option -g window-status-style "fg=#b8b8b8,bg=#282828"
+
+      # active window title colors
+      set-window-option -g window-status-current-style "fg=#f7ca88,bg=#282828"
+
+      # pane border
+      set-option -g pane-border-style "fg=#282828"
+      set-option -g pane-active-border-style "fg=#b8b8b8"
+
+      # message text
+      set-option -g message-style "fg=#e8e8e8,bg=#383838"
+
+      # pane number display
+      set-option -g display-panes-active-colour "#b8b8b8"
+      set-option -g display-panes-colour "#282828"
+
+      # clock
+      set-window-option -g clock-mode-colour "#7cafc2"
+
+      # copy mode highlight
+      set-window-option -g mode-style "fg=#b8b8b8,bg=#383838"
+
+      # bell
+      set-window-option -g window-status-bell-style "fg=#181818,bg=#ab4642"
+
+      # style for window titles with activity
+      set-window-option -g window-status-activity-style "fg=#d8d8d8,bg=#282828"
+
+      # style for command messages
+      set-option -g message-command-style "fg=#e8e8e8,bg=#383838"
+    '';
+
+    plugins = with pkgs; [
+      tmuxPlugins.sensible
+      tmuxPlugins.vim-tmux-navigator
+    ];
+  };
+}
